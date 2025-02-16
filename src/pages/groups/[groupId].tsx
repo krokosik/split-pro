f549b379324b1525de6c15751f9f3510a94ddc50
krokosik <wikkra@gmail.com>
2025-02-16 13:49:19 +0100

    Complete BalanceList

--- src/pages/groups/[groupId].tsx
+++ src/pages/groups/[groupId].tsx
@@ -417,8 +417,6 @@ const BalancePage: NextPageWithUser<{
             <BalanceList
               balances={groupDetailQuery.data?.groupBalances ?? []}
               users={groupDetailQuery.data?.groupUsers.map((gu) => gu.user) ?? []}
-              userId={user.id}
-              isLoading={groupDetailQuery.isLoading}
             />
           </TabsContent>
         </Tabs>
